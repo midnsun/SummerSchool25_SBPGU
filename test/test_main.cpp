da787923b74d496c80b266c1e67d87782117154f
zagriadskov maksim <maksimzagradskrv50910@gmail.com>
2025-06-28 00:55:11 +0300

    Update test_main.cpp

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -3,5 +3,14 @@
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
+
+    int i;
+    for (i = 0; i < argc + 1; i++) {
+        if (argv[i] == nullptr) break;
+        // Printing all the Arguments
+        printf("%s ", argv[i]);
+    }
+    printf("\n");
+
     return RUN_ALL_TESTS();
 }
