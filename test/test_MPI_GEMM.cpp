5ce90e7c1b76b249ce6753e88246fe6950bd6ab1
zagriadskov maksim <maksimzagradskrv50910@gmail.com>
2025-06-28 02:18:05 +0300

    Refactoring

--- test/test_MPI_GEMM.cpp
+++ test/test_MPI_GEMM.cpp
@@ -3,7 +3,12 @@
 
 TEST(MPI_GEMM, MPI_GEMM_TEST)
 {
-	
+	int argc = 1;
+	char** argv = new char*[argc];
+	argv[0] = const_cast<char*>(".\build\bin\test_MPI_GEMM.exe\0");
 
+	ASSERT_NO_THROW(testMPIGemm(argc, argv));
 
+	delete[] argv[0];
+	delete[] argv;
 }
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,29 +1,29 @@
 #include <gtest.h>
 #include <mpi.h>
 
-void testMPI(int rank, int numtasks) {
-    printf("Hello from process = %d, total number of processes: %d\n", rank, numtasks);
-}
+//void testMPI(int rank, int numtasks) {
+//    printf("Hello from process = %d, total number of processes: %d\n", rank, numtasks);
+//}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
-    int i;
-    for (i = 0; i < argc + 1; i++) {
-        if (argv[i] == nullptr) continue;
-        // Printing all the Arguments
-        std::cout << i << " ";
-        printf("%s | ", argv[i]);
-    }
-    printf("\n");
-
-    int numtasks, rank;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-
-    testMPI(rank, numtasks);
-
-    MPI_Finalize();
+//    int i;
+//    for (i = 0; i < argc + 1; i++) {
+//        if (argv[i] == nullptr) continue;
+//        // Printing all the Arguments
+//        std::cout << i << " ";
+//        printf("%s | ", argv[i]);
+//    }
+//    printf("\n");
+//
+//    int numtasks, rank;
+//    MPI_Init(&argc, &argv);
+//    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+//    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+//
+//    testMPI(rank, numtasks);
+//
+//    MPI_Finalize();
 
     return RUN_ALL_TESTS();
 }
